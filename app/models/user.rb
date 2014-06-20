cd1594c528452c4b649313c5c5ed35031b44cd83
Nick L <nicklonz@gmail.com>
2014-06-20 12:24:02 -0400

    Implemented Pundit and basic roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
+# app/models/user.rb
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+
+  def role?(base_role)
+    role == base_role.to_s
+  end
 end
---
+++ app/policies/post_policy.rb
@@ -0,0 +1,9 @@
+# app/policies/post_policy.rb
+
+class PostPolicy < ApplicationPolicy
+
+def index?
+    true
+  end
+
+end
