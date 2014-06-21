392be73b3f98267f92bbe348c7b6d5f9c5967749
Nick L <nicklonz@gmail.com>
2014-06-20 20:50:33 -0400

    Set up post-markdown branch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
-module ApplicationHelper
+# app/helpers/application_helper.rb
+
+def markdown(text)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render text).html_safe
 end
