02a9d57b78023cfebdea5c46af30e9b94b309d6f
Ben Neely <bmneely@gmail.com>
2014-06-22 23:10:32 -0600

    Commenting commenting commenting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-  attr_accessible :body, :post
 
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
