f74e155dd0b9566644026619492a65672b7e9449
Nick L <nicklonz@gmail.com>
2014-06-22 22:33:38 -0400

    Added comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@
 
 class Comment < ActiveRecord::Base
   belongs_to :post
+  belongs_to :user
+  attr_accessible :body, :post
+
+  validates :body, length: { minimum: 5 }, presence: true
+  validates :user, presence: true
 end
